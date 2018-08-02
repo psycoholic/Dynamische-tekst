e8dde264d5fd284d89f372e242061fb16a662d5a
psycoholic <katana_cs@hotmail.com>
2018-08-02 16:32:15 +0200

    DynamicTextArea v.1.0
    First prototype, retrieve's multiple text area's true shortcode attributes. Place the shortcode in the html area where u want to display the text.

--- dynamicTextArea.php
+++ dynamicTextArea.php
@@ -1,40 +1,71 @@
 /***********************
  * Dynamic text area shortcode
  * Retrieves file with same slug as the url, gets the content
+ * Shortcode Syntax = Revers to default text area[textload] Gets first of div class [textload type="textone"], textone,texttwo,textthree,textfour,textfive
  */
-//[textarea]
+//[textload type="textone"]
 function dynamicTextArea( $atts ){
-     // current directory
-    $currentDirectory = getcwd();
-    $currentPage = $_SERVER['REQUEST_URI'];
-     // File retrieve
-    $file = $currentDirectory . '/wp-content/themes/bb-theme-child' . $currentPage . ".txt";
-     // Get filename out of the url
-    $dirName = basename($file);
-    // Build the url
-    $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-    // Convert basename to string and add delimeters for preg_match
-    $dirString = "/" . strval($dirName) . "/";
-    // Replace the mime type for check against url
-    $dirReplaced = str_replace('.txt', '', $dirString);
 
-        // Check the current url of the page against the filename
-        if (preg_match($dirReplaced, $url)) {
-            echo "BaseName and url match" . "<br>";
-            // Retrieve the content out of the directory
-            $document = file_get_contents($file);
-            $firstStep = explode('<div id="text1">', $document);
-            $secondStep = explode('</div>', $firstStep[1]);
-            
-            echo $secondStep[0];
-            // file_put_contents($document, $file, FILE_APPEND);
-        } else {
-            echo "Basename and url dont match". "<br>";
-            $document = the_post();
+        // current directory
+        $currentDirectory = getcwd();
+        $currentPage = $_SERVER['REQUEST_URI'];
+         // File retrieve
+        $file = $currentDirectory . '/wp-content/themes/bb-theme-child' . $currentPage . ".txt";
+         // Get filename out of the url
+        $dirName = basename($file);
+        // Build the url
+        $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+        // Convert basename to string and add delimeters for preg_match
+        $dirString = "/" . strval($dirName) . "/";
+        // Replace the mime type for check against url
+        $dirReplaced = str_replace('.txt', '', $dirString);
+    
+            // Check the current url of the page against the filename
+            if (preg_match($dirReplaced, $url)) {
+                echo "BaseName and url match" . "<br>";
+                // Retrieve the content out of the directory
+                $document = file_get_contents($file);
+                $firstStep = explode('<div id="text1">', $document);
+                $secondStep = explode('</div>', $firstStep[1]);
+                
+                // echo $secondStep[0];
+            } else {
+                echo "Basename and url dont match". "<br>";
+                $document = the_post();
+            }
+    
+        // Check for wich part of the text has to be retrieved.
+
+        extract( shortcode_atts( array(
+            'type' => 'myvalue'
+
+        ), $atts ) );
+
+        switch( $type ){
+            case 'textone': 
+                $output = $secondStep[0];
+                break;
+            case 'texttwo': 
+                $output = $secondStep[1];
+                break;
+            case 'texttwo': 
+                $output = $secondStep[2];
+                break;
+            case 'textthree': 
+                $output = $secondStep[3];
+                break;
+            case 'textfour': 
+                $output = $secondStep[4];
+                break;
+            case 'textfive': 
+                $output = $secondStep[5];
+                break;
+            default:
+                $output = $secondStep[0];
+                break;
         }
 
-        // Filter the content by line break
+        return $output;
 
-	return $explodeDocument;
 }
-add_shortcode( 'textarea', 'dynamicTextArea' );
+add_shortcode( 'textload', 'dynamicTextArea' );
